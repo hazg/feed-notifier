3bd05d991037911a2c2a37c818b482f4dddec077
Aleksandr Aleksandrov <hazg@mail.ru>
2018-11-25 22:51:21 +0300

    desc

--- custom/js/popup.js
+++ custom/js/popup.js
@@ -1,14 +1,22 @@
 $(function(){
-            chrome.storage.local.get("rssFeedList", function(items) {
+         chrome.storage.local.get("rssFeedList", function(items) {
             $.each(items.rssFeedList, function( index, value ) {
-                var nItem = "<li class='list-group-item'><a target='_blank' href='" + value["link"] + "'>" +
-                value["title"] + "</a><div style='font-size:xx-small' class='description'>+"+
-                value["description"]+"+</div></li>";
+                var nItem = "<li class='list-group-item'><a href='#'>" +
+                value["title"] + "</a><div style='display: none;font-size:x-small; padding-bottom:5px;' class='description'>+"+
+                value["description"]+"+<br /><a style='float:right;' href='" + value["link"] + "'>show</a></div></li>";
                 $("#rssList").append(nItem);
-            })
+            });
+
+            $('.list-group-item').click(function(e){
+                var desc = $(this).find('.description')
+                $('.list-group-item .description').not(desc).hide();
+                desc.toggle();
+            });    
             
         });
+
+        
 });
 
-console.log(chrome.alarms.get())
+//console.log(chrome.alarms.get())
 
